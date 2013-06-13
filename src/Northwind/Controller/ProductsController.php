b0033e82a2bcbe4032a8f1d49660076eaa37e9fc
glisandro <gustavomartinez@gmail.com>
2013-06-13 12:29:42 -0300

    remove unnecessary methods

--- src/Northwind/Controller/ProductsController.php
+++ src/Northwind/Controller/ProductsController.php
@@ -25,7 +25,7 @@ class ProductsController implements ControllerProviderInterface{
         $em = $app['em'];
         
         $controllers->match('/', function(Request $request) use ($app, $em) {
-            //echo '<pre>'; var_dump($products[0]->getCategory());exit;
+
             $page = (int) $request->query->get('page', 1);
             $categoryid = (int) $request->query->get('categoryid', 0);
             $orderBy = 'categoryName';
@@ -37,11 +37,9 @@ class ProductsController implements ControllerProviderInterface{
             
             $products = $em->getRepository('Northwind\Entity\Product')->getProductsByCategory($categoryid);
             $productsContainer = new \Northwind\Form\Model\Products($products);
-            //$form = $app['form.factory']->create(new ProductsType(), array('products' => $products));
+
             $form = $app['form.factory']->create(new ProductsType(), $productsContainer);
-           // echo '<pre>';
-           //var_dump($products);
-            //echo '<hr>';
+
             if ($request->isMethod("POST")) {
                 $form->bind($request);
                 
@@ -62,14 +60,11 @@ class ProductsController implements ControllerProviderInterface{
                     $app['session']->getFlashBag()->add('success', 'The product has been edited!');
                     return $app->redirect($app['url_generator']->generate('products',array('categoryid' => $categoryid)));
                 } else {
-                    //var_dump($form->hasErrors());exit;
-                    //$form->addError(new FormError('The form contains errors'));
                     $app['session']->getFlashBag()->add('error', 'The form contains errors.');
                 }
             }
             
             return $app['twig']->render('northwind/products/index.html.twig', array(
-                //'pager' => $products,
                 'categories' => $categories,
                 'categoryid' => $categoryid,
                 'form' => $form->createView()
@@ -77,131 +72,5 @@ class ProductsController implements ControllerProviderInterface{
       
         })->bind('products');
         
- 
-
-
-
-
-       
-        
-        //ADD 
-        $controllers->match('/add', function(Request $request) use ($app, $em) {
-            
-            $product = new Entity\Product();
-            
-            $form = $app['form.factory']->create(new ProductType(),$product);
-            
-            if ($request->isMethod("POST")) {
-                $form->bind($request);
-
-                if ($form->isValid()) {
-                    $em->persist($product);
-                    $em->flush();
-                    
-                    $app['session']->getFlashBag()->add('success', 'A new product has been added !');
-
-                    return $app->redirect($app['url_generator']->generate('products'));
-                } else {
-                    $form->addError(new FormError('The form contains errors'));
-                    //$app['session']->getFlashBag()->add('warning', 'No data was saved');
-
-                }
-            }
-            return $app['twig']->render('northwind/products/add_edit.html.twig', array(
-                'form' => $form->createView()
-            ));
-      
-        })->bind('products_add');
-        
-        // SHOW
-        $controllers->get('/{id}', function($id, Request $request) use ($app, $em) {
-            
-            if($id) {
-                $product = $em->getRepository('Northwind\Entity\Product')->find($id);
-            } else {
-                $app['session']->getFlashBag()->add('error', 'The product does not exist.');
-                return $app->redirect($app['url_generator']->generate('products'));    
-            }
-            
-            return $app['twig']->render('northwind/products/show.html.twig', array(
-                'product' => $product
-            ));
-      
-        })->bind('products_show')->assert('id', '\d+')->value('id', 0);
-        
-        // EDIT
-        $controllers->match('/{id}/edit', function($id, Request $request) use ($app, $em) {
-            
-            if($id) {
-                $product = $em->getRepository('Northwind\Entity\Product')->find($id);
-            } else {
-                $app['session']->getFlashBag()->add('error', 'The product does not exist.');
-                return $app->redirect($app['url_generator']->generate('products'));    
-            }
-            
-            $form = $app['form.factory']->create(new ProductType(),$product);
-            
-            if ($request->isMethod("POST")) {
-                $form->bind($request);
-
-                if ($form->isValid()) {
-                    $em->persist($product);
-                    $em->flush();
-                    
-                    $app['session']->getFlashBag()->add('success', 'The product has been edited!');
-
-                    return $app->redirect($app['url_generator']->generate('products'));
-                } else {
-                    $form->addError(new FormError('The form contains errors'));
-                }
-            }
-            
-            return $app['twig']->render('northwind/products/add_edit.html.twig', array(
-                'form' => $form->createView()
-            ));
-      
-        })->bind('products_edit')->assert('id', '\d+')->value('id', 0);
-        
-        // DELETE
-        $controllers->match('/delete/{id}', function($id, Request $request) use ($app, $em) {
-            
-            $categoryid = (int) $request->query->get('categoryid', 0);
-            var_dump($category);exit;
-            $product = $em->getRepository('Northwind\Entity\Product')->find($id);
-            if (!$product)  {
-                return new Response(sprintf('Product with id %s not found', $id));
-                //throw $request->createNotFoundException(sprintf('Product with id %s not found', $id));
-            }
-            
-            //if($product instanceOf Entity\Product) {
-                $em->remove($product);
-                $em->flush();        
-                
-                $app['session']->getFlashBag()->add('success', 'The product has been deleted.');
-            //} else {
-              //  $app['session']->getFlashBag()->add('error', 'The product does not exist.');
-            //}
-            
-            return $app->redirect($app['url_generator']->generate('products'));    
-            
-            
-        })->bind('products_delete')->assert('id', '\d+')->value('id', 0);
-      
-        
-        
-        return $controllers;
-    }
-    
-    private function deleteCollections($em, $init, $final)
-    {
-        if (empty($init)) {
-            return;
-        }
-
-        if (!$final->getAddresses() instanceof \Doctrine\ORM\PersistentCollection) {
-            foreach ($init['addresses'] as $addr) {
-                $em->remove($addr);
-            }
-        }
     }
 }
